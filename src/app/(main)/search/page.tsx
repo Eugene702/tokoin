4c685c69c6f20c0f0d4f0a3481942216ffdae896
Eugene702 <eugenefeilianputrarangga@gmail.com>
2023-11-19 09:29:27 +0700

    Feat: Menyelesaikan design mobile pada halaman search

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,5 +1,21 @@
+import { Metadata } from "next"
+import dynamic from "next/dynamic"
+
+export const metadata: Metadata = {
+    title: 'Search'
+}
+
+const SearchForm = dynamic(() => import('./searchForm'))
+const ProductList = dynamic(() => import('./productList'))
+
 const page = () => {
-    return <h1>Search</h1>
+    return <main className="w-11/12 mx-auto my-5">
+        <SearchForm />
+
+        <div className="mt-5">
+            <ProductList />
+        </div>
+    </main>
 }
 
 export default page
