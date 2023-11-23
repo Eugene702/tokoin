4f6aff65977f819de4512971480ce718e4e7b775
Eugene702 <eugenefeilianputrarangga@gmail.com>
2023-11-23 12:22:45 +0700

    Feat: Menyelesaikan design mobile untukk halaman profile

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react"
 import { Metadata } from "next"
 import dynamic from "next/dynamic"
 import Image from "next/image"
+import Link from "next/link"
 
 export const metadata: Metadata = {
     title: "Eugene Filian Putra Rangga"
@@ -29,8 +30,28 @@ const page = () => {
                 <SignOutButton />
             </div>
 
-            <div className="mt-5 overflow-x-auto flex gap-x-3">
-
+            <div className="my-10">
+                <h1 className="text-xl font-semibold">Transaction List</h1>
+                <div className="space-y-5 mt-3">
+                    {
+                        Array.from({ length: 5 }).map((_, index) =>
+                            <Link href="/2" className="card card-side bg-base-100 shadow-xl" key={index}>
+                                <figure>
+                                    <Image
+                                        src="https://res.cloudinary.com/ddzlmw5rp/image/upload/v1678305621/samples/people/smiling-man.jpg"
+                                        fill
+                                        alt="Photo Profile"
+                                        sizes="100vw"
+                                        className="!relative !object-cover" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">New movie is released!</h2>
+                                    <p>Click the button to watch on Jetflix app.</p>
+                                </div>
+                            </Link>
+                        )
+                    }
+                </div>
             </div>
         </div>
 
--- src/app/(main)/profile/signOutButton.tsx
+++ src/app/(main)/profile/signOutButton.tsx
@@ -5,6 +5,6 @@ import { showModal } from '@/app/store/reducer/modalReducer'
 
 const SignOutButton = () => {
     const dispatch = useDispatch()
-    return <button className="btn btn-error text-white" onClick={() => dispatch(showModal("#sign-out-confirmation-modal"))}>Sign Out</button>
+    return <button className="btn btn-error text-white rounded-full" onClick={() => dispatch(showModal("#sign-out-confirmation-modal"))}>Sign Out</button>
 }
 export default SignOutButton
