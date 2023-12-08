791375f69bd0b51b6d0437b9ff29b1c9a418309e
Eugene702 <eugenefeilianputrarangga@gmail.com>
2023-12-08 16:11:50 +0700

    Feat: Menyelesaikan authentikasi untuk login user

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -5,14 +5,28 @@ import Image from "next/image"
 import Link from "next/link"
 import { getServerSession } from "next-auth/next"
 import { Session } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import LogoSvg from '@/assets/svg/logo.svg'
 
-const getUser = async (): Promise<Session | null> => {
-    const session = await getServerSession()
+const getCredential = async (): Promise<Session | null> => {
+    const session = await getServerSession(authOptions)
     return session
 }
 
+const getUser = async (email: string) => {
+    if (prisma != undefined) {
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        return user
+    }
+
+    return null
+}
+
 export const generateMetadata = async (): Promise<Metadata> => {
-    const user = await getUser()
+    const user = await getCredential()
 
     return {
         title: user != null ? user.user != null ? user.user.name : "Profile User" : "Profile User"
@@ -23,58 +37,65 @@ const SignOutButton = dynamic(() => import('./signOutButton'))
 const SignOutConfirmationModal = dynamic(() => import('./signOutConfirmationModal'))
 
 const page = async () => {
-    const user = await getUser()
-    
-    if(user == null){
-        return <div className="w-full h-screen flex justify-center items-center">
-            <Link href="/auth/signin" className="btn btn-primary text-white rounded-xl">Login first!</Link>
-        </div>
+    const credential = await getCredential()
+    const noUserTemplate = <div className="w-full h-screen flex justify-center items-center">
+        <Link href="/auth/signin" className="btn btn-primary text-white rounded-xl">Login first!</Link>
+    </div>
+
+    if (credential == null) {
+        return noUserTemplate
     }
 
-    return <Fragment>
-        <div className="w-10/12 mx-auto lg:w-6/12">
-            <div className="flex items-center flex-col gap-y-2 w-full mt-5">
-                <div className="avatar">
-                    <div className="w-40 rounded-full">
-                        <Image
-                            src="https://res.cloudinary.com/ddzlmw5rp/image/upload/v1678305621/samples/people/smiling-man.jpg"
-                            fill
-                            alt="Photo Profile"
-                            sizes="100vw"
-                            className="!relative !object-cover" />
+    if (credential.user != null && credential.user.email != null) {
+        const user = await getUser(credential.user.email)
+
+        return <Fragment>
+            <div className="w-10/12 mx-auto lg:w-6/12">
+                <div className="flex items-center flex-col gap-y-2 w-full mt-5">
+                    <div className="avatar">
+                        <div className="w-40 rounded-full">
+                            <Image
+                                src={ user ? user.image ?? LogoSvg : LogoSvg }
+                                fill
+                                alt="Photo Profile"
+                                sizes="100vw"
+                                className="!relative !object-cover" />
+                        </div>
+                    </div>
+                    <h1 className="text-2xl text-center font-semibold">{ user ? user.name : 'Anonymous' }</h1>
+                    <SignOutButton />
+                </div>
+
+                <div className="my-10">
+                    <h1 className="text-xl font-semibold">Transaction List</h1>
+                    <div className="space-y-5 mt-3">
+                        {
+                            Array.from({ length: 5 }).map((_, index) =>
+                                <Link href="/2" className="card card-side bg-base-100 shadow-xl" key={index}>
+                                    <figure>
+                                        <Image
+                                            src="https://res.cloudinary.com/ddzlmw5rp/image/upload/v1678305621/samples/people/smiling-man.jpg"
+                                            fill
+                                            alt="Photo Profile"
+                                            sizes="100vw"
+                                            className="!relative !object-cover w-6/12" />
+                                    </figure>
+                                    <div className="card-body w-6/12">
+                                        <h2 className="card-title">New movie is released!</h2>
+                                        <p>Click the button to watch on Jetflix app.</p>
+                                    </div>
+                                </Link>
+                            )
+                        }
                     </div>
                 </div>
-                <h1 className="text-2xl text-center font-semibold">Eugene Feilian Putra Rangga</h1>
-                <SignOutButton />
             </div>
 
-            <div className="my-10">
-                <h1 className="text-xl font-semibold">Transaction List</h1>
-                <div className="space-y-5 mt-3">
-                    {
-                        Array.from({ length: 5 }).map((_, index) =>
-                            <Link href="/2" className="card card-side bg-base-100 shadow-xl" key={index}>
-                                <figure>
-                                    <Image
-                                        src="https://res.cloudinary.com/ddzlmw5rp/image/upload/v1678305621/samples/people/smiling-man.jpg"
-                                        fill
-                                        alt="Photo Profile"
-                                        sizes="100vw"
-                                        className="!relative !object-cover w-6/12" />
-                                </figure>
-                                <div className="card-body w-6/12">
-                                    <h2 className="card-title">New movie is released!</h2>
-                                    <p>Click the button to watch on Jetflix app.</p>
-                                </div>
-                            </Link>
-                        )
-                    }
-                </div>
-            </div>
-        </div>
-
-        <SignOutConfirmationModal />
-    </Fragment>
+            <SignOutConfirmationModal />
+        </Fragment>
+    }else{
+        return noUserTemplate
+    }
 }
 
 export default page
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,18 @@
-import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
+import prisma from "../../../../../prisma/db"
 
-const handler = NextAuth({
-    secret: process.env.NEXTAUTH_SECRET,
+export const authOptions: NextAuthOptions = {
+    // secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: `${process.env.GOOGLE_CLIENT_ID}`,
             clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`
         })
     ]
-})
+}
 
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
